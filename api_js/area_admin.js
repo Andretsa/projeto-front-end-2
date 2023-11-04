e2f3a22357bae29ee6bbc9a5be3199fdab1206f1
Andretsa <106777099+Andretsa@users.noreply.github.com>
2023-11-04 20:46:23 -0300

    Add files via upload
    Primeiro commit

---
+++ api_js/area_admin.js
@@ -0,0 +1,6 @@
+const adminLink = document.getElementById("admin-link");
+const adminArea = document.getElementById("admin-area");
+
+adminLink.addEventListener("click", function() {
+    adminArea.style.display = "block";
+});
