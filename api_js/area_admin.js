fc5af7bb386e39c56e7958cfd0899911b55e6ae5
Andretsa <106777099+Andretsa@users.noreply.github.com>
2023-11-13 22:26:55 -0300

    Delete api_js directory

--- api_js/area_admin.js
+++
@@ -1,6 +0,0 @@
-const adminLink = document.getElementById("admin-link");
-const adminArea = document.getElementById("admin-area");
-
-adminLink.addEventListener("click", function() {
-    adminArea.style.display = "block";
-});
